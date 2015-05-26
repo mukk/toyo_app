7de3f2303317394b48775b4d93e57868c5eea08c
mukk <kkoregatsu@gmail.com>
2015-05-26 16:05:42 +0900

    特に意味はないコミット

--- Example/AppDelegate.h
+++ Example/AppDelegate.h
@@ -1,8 +1,6 @@
 // Copyright 2007-2014 metaio GmbH. All rights reserved.
 #import <UIKit/UIKit.h>
 
-@class ViewController;
-
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
